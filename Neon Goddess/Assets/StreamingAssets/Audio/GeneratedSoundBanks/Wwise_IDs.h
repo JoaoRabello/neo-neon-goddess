e306b58e04438db47778023893c63b10896b34db
zihavellar <86857945+zihavellar@users.noreply.github.com>
2022-07-18 22:12:05 -0300

    SD03 - Bus Controller Game Parametre

---
+++ Neon Goddess/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,52 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID ACCESSIBILITYBUSCONTROLLER = 2133121051U;
+        static const AkUniqueID AMBIENCEBUSCONTROLLER = 3767945453U;
+        static const AkUniqueID AWARENESSBUSCONTROLLER = 4148718858U;
+        static const AkUniqueID DIALOGUEBUSCONTROLLER = 1970181751U;
+        static const AkUniqueID INFOBUSCONTROLLER = 3037820317U;
+        static const AkUniqueID MASTERBUSCONTROLLER = 1444919967U;
+        static const AkUniqueID MUSICBUSCONTROLLER = 80573052U;
+        static const AkUniqueID SFXBUSCONTROLLER = 62014404U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAINSOUNDBANK = 534561221U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID ACCESSIBILITYBUS = 639560775U;
+        static const AkUniqueID AMBIENCEBUS = 612772313U;
+        static const AkUniqueID AWARENESSBUS = 3086158442U;
+        static const AkUniqueID DIALOGUEBUS = 309504875U;
+        static const AkUniqueID INFOBUS = 151322537U;
+        static const AkUniqueID MASTERBUS = 835198467U;
+        static const AkUniqueID MUSICBUS = 2886307548U;
+        static const AkUniqueID SFXBUS = 3803850708U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
