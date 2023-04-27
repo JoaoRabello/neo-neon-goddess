9b71f761e9670102e3c26b833fa8000b477402eb
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2023-04-27 20:49:52 -0300

    Feat: Ledge Grab implemented

--- Neon Goddess/Assets/_Prototypes/10. Tank Movement/LedgeGrab.cs
+++ Neon Goddess/Assets/_Prototypes/10. Tank Movement/LedgeGrab.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,14 @@ using UnityEngine.InputSystem;
 public class LedgeGrab : MonoBehaviour
 {
     [SerializeField] private float _grabRange;
+    [SerializeField] private float _downRange;
     [SerializeField] private LayerMask _ledgeLayerMask;
+    [SerializeField] private Animator _playerAnimator;
+    [SerializeField] private GameObject _parent;
+    [SerializeField] private List<Collider> _colliders = new List<Collider>();
+    
     private InputActions _inputActions;
+    private bool _isClimbing;
 
     private void Awake()
     {
@@ -27,14 +34,54 @@ public class LedgeGrab : MonoBehaviour
         
         _inputActions.Disable();
     }
-    
+
+    private void OnAnimatorMove()
+    {
+        if (!_isClimbing) return;
+        
+        _parent.transform.position += _playerAnimator.deltaPosition;
+    }
+
     private void LedgePerformed(InputAction.CallbackContext obj)
     {
         var hasLedgeInFront = Physics.Raycast(transform.position, transform.forward, out var hit, _grabRange, _ledgeLayerMask);
 
         if (!hasLedgeInFront) return;
         if (!hit.collider.TryGetComponent<Collider>(out var component)) return;
+
+        StartCoroutine(PlayAndWaitForAnim(_playerAnimator, "Climbing"));
+    }
+    
+    public IEnumerator PlayAndWaitForAnim(Animator targetAnim, string stateName)
+    {
+        _isClimbing = true;
+        targetAnim.Play(stateName);
         
-        Debug.Log(component.name);
+        foreach (var collider in _colliders)
+        {
+            collider.enabled = false;
+        }
+
+        while (!targetAnim.GetCurrentAnimatorStateInfo(0).IsName(stateName))
+        {
+            yield return null;
+        }
+
+        while ((targetAnim.GetCurrentAnimatorStateInfo(0).normalizedTime) % 1 < 0.99f)
+        {
+            yield return null;
+        }
+
+        _isClimbing = false;
+        foreach (var collider in _colliders)
+        {
+            collider.enabled = true;
+        }
+        
+        var hasLedgeUnder = Physics.Raycast(transform.position, Vector3.down, out var hit, _downRange, _ledgeLayerMask);
+
+        if (!hasLedgeUnder) yield break;
+
+        transform.position -= transform.position - hit.point;
     }
 }
