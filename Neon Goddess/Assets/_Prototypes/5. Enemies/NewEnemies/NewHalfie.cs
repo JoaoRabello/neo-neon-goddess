7ae300cfb6be3f1271b693bfe36a50e5ef32f5c4
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2023-05-03 00:04:12 -0300

    Feat: Halfie jump + base return

--- Neon Goddess/Assets/_Prototypes/5. Enemies/NewEnemies/NewHalfie.cs
+++ Neon Goddess/Assets/_Prototypes/5. Enemies/NewEnemies/NewHalfie.cs
@@ -6,23 +6,55 @@ using UnityEngine;
 public class NewHalfie : MonoBehaviour
 {
     [SerializeField] private LayerMask _playerLayerMask;
+    [SerializeField] private float _attackRange;
+    [SerializeField] private float _forgetRange;
     [SerializeField] private float _range;
     [SerializeField] private float _speed;
 
+    private float _currentSpeed;
+    
     private bool _foundPlayer;
     private Transform _player;
+    private Vector3 _basePosition;
+
+    private bool _attackCooldown;
+
+    private void Start()
+    {
+        _currentSpeed = _speed;
+        _basePosition = transform.position;
+    }
 
     void Update()
     {
+        var distanceToPlayer = _player ? Vector3.Distance(transform.position, _player.position) : 0;
+        
+        if (distanceToPlayer >= _forgetRange)
+        {
+            _foundPlayer = false;
+            transform.position = Vector3.MoveTowards(transform.position, _basePosition, _currentSpeed * Time.deltaTime);
+            
+            RotateTowards(_basePosition);
+            return;
+        }
+        
+        if (_attackCooldown) return;
+        
         Collider[] playerColliders = new Collider[3];
         var size = Physics.OverlapSphereNonAlloc(transform.position, _range, playerColliders, _playerLayerMask);
 
         if (_foundPlayer)
         {
-            transform.position = Vector3.MoveTowards(transform.position, _player.position, _speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, _player.position, _currentSpeed * Time.deltaTime);
             transform.position = new Vector3(transform.position.x, 0.2f, transform.position.z);
-
-            transform.forward = (_player.position - transform.position).normalized;
+            
+            RotateTowards(_player.position);
+            
+            if (Vector3.Distance(transform.position, _player.position) <= _attackRange)
+            {
+                _currentSpeed = 20;
+                StartCoroutine(AttackCooldown());
+            }
         }
         else
         {
@@ -33,9 +65,31 @@ public class NewHalfie : MonoBehaviour
         }
     }
 
+    private void RotateTowards(Vector3 targetPos)
+    {
+        transform.forward = (targetPos - transform.position).normalized;
+    }
+
+    private IEnumerator AttackCooldown()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        _attackCooldown = true;
+        _currentSpeed = _speed;
+
+        yield return new WaitForSeconds(3);
+        
+        _attackCooldown = false;
+    }
+
     private void OnDrawGizmos()
     {
-        Gizmos.color = new Color(1, 0, 0, 0.2f);
+        Gizmos.color = new Color(1, 1, 0, 0.2f);
         Gizmos.DrawSphere(transform.position, _range);
+        Gizmos.color = new Color(1, 0, 0, 0.2f);
+        Gizmos.DrawSphere(transform.position, _attackRange);
+        
+        Gizmos.color = new Color(0, 1, 0, 0.2f);
+        Gizmos.DrawSphere(transform.position, _forgetRange);
     }
 }
