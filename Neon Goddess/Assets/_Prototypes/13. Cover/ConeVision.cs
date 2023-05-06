d54a4262ba0594d7d9398f3e8779c373ec5fd5ea
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2023-05-06 14:57:39 -0300

    Feat: Cone vision for player detection

---
+++ Neon Goddess/Assets/_Prototypes/13. Cover/ConeVision.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConeVision : MonoBehaviour
+{
+    [SerializeField] private float _visionRange;
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private LayerMask _playerMask;
+
+    private Transform _playerTransform;
+    private bool _playerIsVisible;
+    
+    void Update()
+    {
+        RotateAroundItself();
+        TryFindPlayer();
+    }
+
+    private void RotateAroundItself()
+    {
+        transform.RotateAround(transform.position, transform.up, Time.deltaTime * _rotationSpeed);
+    }
+
+    private void TryFindPlayer()
+    {
+        var myPos = transform.position;
+        var results = new Collider[2];
+        var size = Physics.OverlapSphereNonAlloc(myPos, _visionRange, results, _playerMask);
+
+        if (size <= 0)
+        {
+            _playerTransform = null;
+            return;
+        }
+
+        _playerTransform = results[0].transform;
+        var playerPos = results[0].transform.position;
+        var directionToPlayer = (playerPos - myPos).normalized;
+        var dotResult = Vector3.Dot(transform.forward, directionToPlayer);
+
+        if (dotResult >= 0.5f)
+        {
+            _playerIsVisible = true;
+        }
+        else
+        {
+            _playerIsVisible = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 1, 0, 0.1f);
+        Gizmos.DrawSphere(transform.position, _visionRange);
+        
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position, transform.forward * _visionRange);
+
+        if (!_playerTransform) return;
+        
+        Gizmos.color = _playerIsVisible ? Color.green : Color.red;
+        Gizmos.DrawRay(transform.position, (_playerTransform.position - transform.position));
+    }
+}
