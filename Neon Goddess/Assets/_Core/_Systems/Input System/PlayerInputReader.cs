c8fe06bef6b95f8e1e5289d86015702a1c331f5e
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2023-06-03 15:16:16 -0300

    Feat: PlayerInputReader fully commented

--- Neon Goddess/Assets/_Core/_Systems/Input System/PlayerInputReader.cs
+++ Neon Goddess/Assets/_Core/_Systems/Input System/PlayerInputReader.cs
@@ -7,6 +7,11 @@ using UnityEngine.InputSystem;
 
 namespace Inputs
 {
+    /// <summary>
+    /// Singleton that reads the player Inputs and call events in response
+    /// <br></br>Every script subscribed to these events will receive messages when inputs are done
+    /// <example> <c>InteractPerformed</c> Action is called when the Interaction button is pressed</example>
+    /// </summary>
     public class PlayerInputReader : MonoBehaviour
     {
         public static PlayerInputReader Instance;
@@ -50,11 +55,29 @@ namespace Inputs
             _inputActions.Disable();
         }
 
+        /// <summary>
+        /// Called when Interaction Input is performed
+        /// </summary>
         public Action InteractPerformed;
+        /// <summary>
+        /// Called when Movement Input is performed and passes the Vector2 input
+        /// </summary>
         public Action<Vector2> MovementPerformed;
+        /// <summary>
+        /// Called when Movement Input is canceled (button up)
+        /// </summary>
         public Action MovementCanceled;
+        /// <summary>
+        /// Called when Crouch Input is performed
+        /// </summary>
         public Action CrouchPerformed;
+        /// <summary>
+        /// Called when Aim Input is performed
+        /// </summary>
         public Action AimPerformed;
+        /// <summary>
+        /// Called when Shoot Input is performed
+        /// </summary>
         public Action ShootPerformed;
         
         private void OnInteractPerformed(InputAction.CallbackContext context)
