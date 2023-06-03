dc353dc9b71c8822e7ea706b841dd802c6909805
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2023-06-03 15:34:15 -0300

    Feat: Better character animator comments

--- Neon Goddess/Assets/_Core/_Systems/Player General/CharacterAnimator.cs
+++ Neon Goddess/Assets/_Core/_Systems/Player General/CharacterAnimator.cs
@@ -4,25 +4,47 @@ using UnityEngine;
 
 namespace Animations
 {
+    /// <summary>
+    /// Component that controls animators due to external calls
+    /// </summary>
     public class CharacterAnimator : MonoBehaviour
     {
+        [Tooltip("Animator controller that controls the current character animations")]
         [SerializeField] private Animator _animatorController;
 
+        /// <summary>
+        /// Directly plays an animation when called
+        /// </summary>
         public void PlayAnimation()
         {
             
         }
 
+        /// <summary>
+        /// Sets the parameter to the desired value
+        /// </summary>
+        /// <param name="parameterName">Animator parameter name</param>
+        /// <param name="value">Value to pass as bool</param>
         public void SetParameterValue(string parameterName, bool value)
         {
             _animatorController.SetBool(parameterName, value);
         }
         
+        /// <summary>
+        /// Sets the parameter to the desired value
+        /// </summary>
+        /// <param name="parameterName">Animator parameter name</param>
+        /// <param name="value">Value to pass as float</param>
         public void SetParameterValue(string parameterName, float value)
         {
             _animatorController.SetFloat(parameterName, value);
         }
         
+        /// <summary>
+        /// Sets the parameter to the desired value
+        /// </summary>
+        /// <param name="parameterName">Animator parameter name</param>
+        /// <param name="value">Value to pass as int</param>
         public void SetParameterValue(string parameterName, int value)
         {
             _animatorController.SetInteger(parameterName, value);
