9aacad0aa481cd47a256711307b6c0c7af57f902
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2023-06-05 21:10:58 -0300

    Feat: New Attack system using aim system

--- Neon Goddess/Assets/_Core/_Systems/Player/Combat/AimSystem.cs
+++ Neon Goddess/Assets/_Core/_Systems/Player/Combat/AimSystem.cs
@@ -1,101 +1,117 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Animations;
 using Inputs;
 using Player;
 using UnityEngine;
 
-public class AimSystem : MonoBehaviour
+namespace Combat
 {
-    private enum AimDirection
+    public class AimSystem : MonoBehaviour
     {
-        Up,
-        Front,
-        Down
-    }
-
-    [Tooltip("Our custom animator")]
-    [SerializeField] private CharacterAnimator _animator;
-    
-    private AimDirection _currentAimingDirection;
-    private Vector3 _currentAimingDirectionVector3;
-    private bool _isAiming;
-
-    private void OnEnable()
-    {
-        PlayerInputReader.Instance.AimPerformed += AimPerformed;
-        PlayerInputReader.Instance.AimCanceled += AimCanceled;
-        PlayerInputReader.Instance.MovementPerformed += MovePerformed;
-        PlayerInputReader.Instance.MovementCanceled += MoveCanceled;
-    }
-    
-    private void OnDisable()
-    {
-        PlayerInputReader.Instance.AimPerformed -= AimPerformed;
-        PlayerInputReader.Instance.AimCanceled -= AimCanceled;
-        PlayerInputReader.Instance.MovementPerformed -= MovePerformed;
-        PlayerInputReader.Instance.MovementCanceled -= MoveCanceled;
-    }
-
-    private void AimPerformed()
-    {
-        _isAiming = true;
-        PlayerStateObserver.Instance.OnAimStart();
-        
-        _animator.SetParameterValue("isAiming", true);
-    }
-    
-    private void AimCanceled()
-    {
-        _isAiming = false;
-        PlayerStateObserver.Instance.OnAimEnd();
-        
-        _animator.SetParameterValue("isAiming", false);
-    }
-    
-    private void MovePerformed(Vector2 movementInput)
-    {
-        _currentAimingDirection = movementInput.y switch
+        public enum AimDirection
         {
-            >= 0.1f => AimDirection.Up,
-            <= -0.1f => AimDirection.Down,
-            _ => AimDirection.Front
-        };
-    }
-    
-    private void MoveCanceled()
-    {
-        _currentAimingDirection = AimDirection.Front;
-    }
+            Up,
+            Front,
+            Down
+        }
 
-    private void Update()
-    {
-        if (!_isAiming) return;
+        [Tooltip("Our custom animator")] [SerializeField]
+        private CharacterAnimator _animator;
 
-        switch (_currentAimingDirection)
+        private AimDirection _currentAimingDirection;
+        private Vector3 _currentAimingDirectionVector3;
+        private bool _isAiming;
+
+        public bool IsAiming => _isAiming;
+        public AimDirection CurrentAimingDirection => _currentAimingDirection;
+
+        private void OnEnable()
         {
-            case AimDirection.Up:
-                _currentAimingDirectionVector3 = transform.forward + Vector3.up * 0.75f;
-                _animator.SetParameterValue("aimDirection", 0f);
-                break;
-            case AimDirection.Front:
-                _currentAimingDirectionVector3 = transform.forward;
-                _animator.SetParameterValue("aimDirection", 0.5f);
-                break;
-            case AimDirection.Down:
-                _currentAimingDirectionVector3 = transform.forward + Vector3.down * 0.75f;
-                _animator.SetParameterValue("aimDirection", 1f);
-                break;
+            PlayerInputReader.Instance.AimPerformed += AimPerformed;
+            PlayerInputReader.Instance.AimCanceled += AimCanceled;
+            PlayerInputReader.Instance.MovementPerformed += MovePerformed;
+            PlayerInputReader.Instance.MovementCanceled += MoveCanceled;
+        }
+
+        private void OnDisable()
+        {
+            PlayerInputReader.Instance.AimPerformed -= AimPerformed;
+            PlayerInputReader.Instance.AimCanceled -= AimCanceled;
+            PlayerInputReader.Instance.MovementPerformed -= MovePerformed;
+            PlayerInputReader.Instance.MovementCanceled -= MoveCanceled;
+        }
+
+        private void AimPerformed()
+        {
+            _isAiming = true;
+            PlayerStateObserver.Instance.OnAimStart();
+
+            _animator.SetParameterValue("isAiming", true);
+        }
+
+        private void AimCanceled()
+        {
+            _isAiming = false;
+            PlayerStateObserver.Instance.OnAimEnd();
+
+            _animator.SetParameterValue("isAiming", false);
+        }
+
+        private void MovePerformed(Vector2 movementInput)
+        {
+            _currentAimingDirection = movementInput.y switch
+            {
+                >= 0.1f => AimDirection.Up,
+                <= -0.1f => AimDirection.Down,
+                _ => AimDirection.Front
+            };
+        }
+
+        private void MoveCanceled()
+        {
+            _currentAimingDirection = AimDirection.Front;
+        }
+
+        private void Update()
+        {
+            if (!_isAiming) return;
+
+            _currentAimingDirectionVector3 = _currentAimingDirection.ToVector3(transform.forward);
+            
+            switch (_currentAimingDirection)
+            {
+                case AimDirection.Up:
+                    _animator.SetParameterValue("aimDirection", 0f);
+                    break;
+                case AimDirection.Front:
+                    _animator.SetParameterValue("aimDirection", 0.5f);
+                    break;
+                case AimDirection.Down:
+                    _animator.SetParameterValue("aimDirection", 1f);
+                    break;
+            }
+        }
+
+        //TODO: Remove debug gizmos
+        private void OnDrawGizmos()
+        {
+            if (!_isAiming) return;
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(transform.position + Vector3.up * 0.5f, _currentAimingDirectionVector3 * 3);
         }
     }
-
-    //TODO: Remove debug gizmos
-    private void OnDrawGizmos()
+    
+    public static class AimDirectionExtensions
     {
-        if (!_isAiming) return;
-        
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(transform.position + Vector3.up * 0.5f, _currentAimingDirectionVector3 * 3);
+        public static Vector3 ToVector3(this AimSystem.AimDirection aimDirection, Vector3 forward)
+        {
+            return aimDirection switch
+            {
+                AimSystem.AimDirection.Up => forward + Vector3.up * 0.75f,
+                AimSystem.AimDirection.Front => forward,
+                AimSystem.AimDirection.Down => forward + Vector3.down * 0.75f,
+                _ => forward
+            };
+        }
     }
 }
