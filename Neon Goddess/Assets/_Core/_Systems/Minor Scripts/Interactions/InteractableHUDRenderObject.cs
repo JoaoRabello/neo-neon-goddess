09a08e7e8e7059cd7c686e6dd3417c9d275cb795
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2023-08-15 18:47:27 -0300

    Feat: Animações de HUD para interagiveis

--- Neon Goddess/Assets/_Core/_Systems/Minor Scripts/Interactions/InteractableHUDRenderObject.cs
+++ Neon Goddess/Assets/_Core/_Systems/Minor Scripts/Interactions/InteractableHUDRenderObject.cs
@@ -6,12 +6,18 @@ public class InteractableHUDRenderObject : MonoBehaviour
 {
     [SerializeField] private RectTransform _rectTransform;
     [SerializeField] private Image _image;
+    [SerializeField] private Animator _animator;
     
     [Header("Icons")]
+    [SerializeField] private string _openDoorAnimName;
     [SerializeField] private Sprite _openDoorIcon;
+    [SerializeField] private string _closedDoorAnimName;
     [SerializeField] private Sprite _closedDoorIcon;
+    [SerializeField] private string _shutDoorAnimName;
     [SerializeField] private Sprite _shutDoorIcon;
+    [SerializeField] private string _newInteractionAnimName;
     [SerializeField] private Sprite _newInteractionIcon;
+    [SerializeField] private string _oldInteractionAnimName;
     [SerializeField] private Sprite _oldInteractionIcon;
     
     private IInteractable _interactable;
@@ -36,19 +42,23 @@ public class InteractableHUDRenderObject : MonoBehaviour
             case IInteractable.InteractableType.Common:
             case IInteractable.InteractableType.Dialogue:
                 _image.sprite = interactable.HasInteractedOnce() ? _oldInteractionIcon : _newInteractionIcon;
+                _animator.Play(interactable.HasInteractedOnce() ? _oldInteractionAnimName : _newInteractionAnimName);
                 break;
             case IInteractable.InteractableType.Door:
                 if (interactable.IsLocked())
                 {
                     _image.sprite = _closedDoorIcon;
+                    _animator.Play(_closedDoorAnimName);
                 }
                 else
                 {
                     _image.sprite = _openDoorIcon;
+                    _animator.Play(_openDoorAnimName);
                 }
                 break;
             case IInteractable.InteractableType.ShutDoor:
                 _image.sprite = _shutDoorIcon;
+                _animator.Play(_shutDoorAnimName);
                 break;
         }
         
