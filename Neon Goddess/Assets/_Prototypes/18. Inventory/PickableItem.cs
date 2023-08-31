05e4767ba1456a0d993e912b5b8ad7194e0a37f0
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2023-08-30 22:16:02 -0300

    Fix: Itens agora têm sua interação diretamente ligada ao item

--- Neon Goddess/Assets/_Prototypes/18. Inventory/PickableItem.cs
+++ Neon Goddess/Assets/_Prototypes/18. Inventory/PickableItem.cs
@@ -4,42 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PickableItem : MonoBehaviour
+public class PickableItem : MonoBehaviour, IInteractable
 {
     [SerializeField] private Item _item;
     [SerializeField] private int _amount;
     [SerializeField] private GameObject dialogue;
+    [SerializeField] private Dialogue _dialogue;
     private InventoryHolder _playerInventoryHolder;
-    private InputActions _inputActions;
-
-    private void Awake()
-    {
-        _inputActions = new InputActions();
-    }
-
-    private void OnEnable()
-    {
-        _inputActions.Prototype.Interact.performed += OnInteractPerformed;
-        
-        _inputActions.Enable();
-    }
-
-    private void OnDisable()
-    {
-        _inputActions.Prototype.Interact.performed -= OnInteractPerformed;
-        
-        _inputActions.Disable();
-    }
-    
-    private void OnInteractPerformed(InputAction.CallbackContext context)
-    {
-        if (_playerInventoryHolder is null) return;
-
-        if (!_playerInventoryHolder.TryAddItem(_item, _amount)) return;
-        
-        Destroy(gameObject);
-        dialogue.SetActive(false);
-    }
     
     private void OnTriggerEnter(Collider other)
     {
@@ -55,5 +26,33 @@ public class PickableItem : MonoBehaviour
         {
             _playerInventoryHolder = null;
         }
+    }
+
+    public Action<IInteractable> OnInteractUpdateIcon { get; set; }
+    public Action<IInteractable> OnStateChangeUpdateIcon { get; set; }
+    public void Interact()
+    {
+        if (_playerInventoryHolder is null) return;
+
+        if (!_playerInventoryHolder.TryAddItem(_item, _amount)) return;
+        
+        ChatDialogueReader.Instance.PlayDialogue(_dialogue);
+        
+        Destroy(gameObject);
+    }
+
+    public IInteractable.InteractableType GetInteractableType()
+    {
+        return IInteractable.InteractableType.Common;
+    }
+
+    public bool HasInteractedOnce()
+    {
+        return false;
+    }
+
+    public bool IsLocked()
+    {
+        return false;
     }
 }
